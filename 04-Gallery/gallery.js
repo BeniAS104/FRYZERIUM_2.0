01e14d2fa5ded4ba48fd21c61384f13b2d0ba87e
BeniAS104 <beniuu104@gmail.com>
2024-05-18 21:57:27 +0200

    stashed changes incoming

---
+++ 04-Gallery/gallery.js
@@ -0,0 +1,26 @@
+let swiper = new Swiper(".slide-container", {
+    slidesPerView: 4,
+    spaceBetween: 15,
+    loop: true,
+    centerSlide: "true",
+    grabCursor: "true",
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      520: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 3,
+      },
+      1000: {
+        slidesPerView: 4,
+      },
+    },
+  });
